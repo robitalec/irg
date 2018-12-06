6287fb0bd42f8268e4ac3ba97165c2a8c7dc7a17
Alec Robitaille <robit.alec@gmail.com>
2018-12-06 13:10:47 -0800

    check cols exist filter_qa

--- R/filter.R
+++ R/filter.R
@@ -6,12 +6,25 @@
 #' @param qa QA column. default is 'SummaryQA'.
 #' @param good values which correspond to quality pixels. default is 0 and 1.
 #'
+#' @import data.table
+#'
 #' @export
 #'
 #' @examples
 #' filter_qa(DT, qa = 'SummaryQA', good = c(0, 1))
 filter_qa <- function(DT, qa = 'SummaryQA', good = c(0, 1)) {
+
+	if (!('NDVI' %in% colnames(DT))) {
+		stop('NDVI column not found in DT')
+	}
+
+	if (!(qa %in% colnames(DT))) {
+		stop('QA column not found in DT')
+	}
+
+
 	DT[(qa) %in% good, good := TRUE][is.na(good), good := FALSE]
 	DT[(good), filtered := NDVI]
 	DT[!(good), filtered := NA]
+
 }
