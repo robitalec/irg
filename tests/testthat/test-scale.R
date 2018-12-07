8d43d75119f56bfe1a570f47ec6dfe4d67dc2120
Alec Robitaille <robit.alec@gmail.com>
2018-12-06 16:29:04 -0800

    test scale

---
+++ tests/testthat/test-scale.R
@@ -0,0 +1,21 @@
+context("test-scale")
+
+ndvi <- fread(system.file("extdata", "ndvi.csv", package = "irg"))
+
+### scale_ndvi ###########################################
+test_that("scale_ndvi works", {
+
+	# Columns mising are detected
+	expect_error(scale_ndvi(ndvi),
+							 'missing one of "rolled", "winter", "top". did you filter?')
+})
+
+### scale_doy ###########################################
+test_that("scale_doy works", {
+
+	# Columns mising are detected
+	expect_error(scale_doy(ndvi, doy = 'potato'),
+							 'doy column not found in DT')
+
+
+})
