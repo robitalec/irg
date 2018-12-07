188a2330ffc069f90dc40a4e48b269bdd9a62b9e
Alec Robitaille <robit.alec@gmail.com>
2018-12-06 16:58:59 -0800

    test test test

--- tests/testthat/test-scale.R
+++ tests/testthat/test-scale.R
@@ -16,6 +16,14 @@ test_that("scale_ndvi works", {
 	expect_true("winter" %in% colnames(ndvi))
 	expect_true("rolled" %in% colnames(ndvi))
 	expect_true("top" %in% colnames(ndvi))
+
+	# Was scaled added
+	scale_ndvi(ndvi)
+	expect_true("scaled" %in% colnames(ndvi))
+
+	# Did it rescale to 0-1?
+	expect_lte(max(ndvi$scaled, na.rm = TRUE), 1)
+	expect_gte(min(ndvi$scaled, na.rm = TRUE), 0)
 })
 
 ### scale_doy ###########################################
@@ -25,8 +33,10 @@ test_that("scale_doy works", {
 	expect_error(scale_doy(ndvi, doy = 'potato'),
 							 'doy column not found in DT')
 
-
+	# Was t added
 	expect_true("t" %in% colnames(scale_doy(ndvi)))
 
-
+	# Did it rescale to 0-1?
+	expect_lte(max(ndvi$t), 1)
+	expect_gte(min(ndvi$t), 0)
 })
