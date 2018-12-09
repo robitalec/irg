34ceb7a2ed6c15fcc952215230ace4e3de11eadc
Alec Robitaille <robit.alec@gmail.com>
2018-12-09 11:10:47 -0800

    todo: clarify naming for model functions

--- R/fit_ndvi.R
+++ R/fit_ndvi.R
@@ -1,4 +1,10 @@
+# TODO: clarify naming: we need to estimate parameters with a model, use those parameters to model NDVI for the whole year, and meta calc_smooth_ndvi
+
+
+
 #' Fit model
+#'
+#' Fit double logistic model to NDVI time series given parameters estimated with fit_params
 #'
 #' @param DT
 #'
@@ -13,6 +19,8 @@ fit_model <- function(DT) {
 
 #' Fit parameters
 #'
+#' Model estimated parameters
+#'
 #' @param DT
 #'
 #' @return
@@ -20,5 +28,22 @@ fit_model <- function(DT) {
 #'
 #' @examples
 fit_params <- function(DT) {
+
+}
+
+
+#' Fit NDVI
+#'
+#' Fit NDVI time series using double logistic model.
+#'
+#'
+#'
+#' @param DT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_smooth_ndvi <- function(DT) {
 
 }
