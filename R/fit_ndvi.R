43dd2f421f3b7d9f1d471a02815b972fbfda9797
Alec Robitaille <robit.alec@gmail.com>
2018-12-09 11:05:14 -0800

    fst fit_ndvi

---
+++ R/fit_ndvi.R
@@ -0,0 +1,24 @@
+#' Fit model
+#'
+#' @param DT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fit_model <- function(DT) {
+
+}
+
+
+#' Fit parameters
+#'
+#' @param DT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fit_params <- function(DT) {
+
+}
