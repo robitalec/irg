c3c551f0dbd5a18040e1fee9ccdc841102958cd2
Alec Robitaille <robit.alec@gmail.com>
2018-12-09 16:26:51 -0800

    clarify naming

---
+++ R/model.R
@@ -0,0 +1,28 @@
+#' Model parameters
+#'
+#' Estimated parameters for fitting double logistic curve.
+#'
+#' @param DT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+model_params <- function(DT) {
+
+}
+
+
+#' Model NDVI time series
+#'
+#' Fit double logistic model to NDVI time series given parameters estimated with fit_params
+#'
+#' @param DT
+#'
+#' @return
+#' @export
+#'
+#' @examples
+model_ndvi <- function(DT) {
+
+}
