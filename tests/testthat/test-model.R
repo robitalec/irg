4628563f74ab1cb75d1aed475ec7cd05dc3e503a
Alec Robitaille <robit.alec@gmail.com>
2018-12-10 18:47:19 -0800

    test model_ndvi

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -25,7 +25,42 @@ test_that("model_params works", {
 		model_params(ndvi, xmidS = 0.5, xmidA = 0.5, scalS = 0.5),
 		'scalA column not found in DT'
 	)
+})
+
+mods <- model_params(
+	ndvi,
+	xmidS = 0.44,
+	xmidA = 0.80,
+	scalS = 0.05,
+	scalA = 0.01
+)
 
 
+test_that("model_ndvi works", {
+
+
+	copyMods <- copy(mods)[, xmidS := NULL]
+	expect_error(
+		model_ndvi(copyMods),
+		'xmidS column not found in DT'
+	)
+
+	copyMods <- copy(mods)[, xmidA := NULL]
+	expect_error(
+		model_ndvi(copyMods),
+		'xmidA column not found in DT'
+	)
+
+	copyMods <- copy(mods)[, scalS := NULL]
+	expect_error(
+		model_ndvi(copyMods),
+		'scalS column not found in DT'
+	)
+
+	copyMods <- copy(mods)[, scalA := NULL]
+	expect_error(
+		model_ndvi(copyMods),
+		'scalA column not found in DT'
+	)
 
 })
