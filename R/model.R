27346430f7425ad2ead8f3d222ddf513e1a970e4
Alec Robitaille <robit.alec@gmail.com>
2018-12-10 16:25:40 -0800

    add model formula in man

--- R/model.R
+++ R/model.R
@@ -2,7 +2,21 @@
 #'
 #' Estimated parameters for fitting double logistic curve.
 #'
-#' @param DT
+#' Default value for the year column is 'yr'.
+#'
+#' The id parameter is used to split between sampling units. This may be a point id, polygon id, pixel id, etc. depending on your analysis. This should match the id provided to filtering functions.
+#'
+#' Arguments xmidS, xmidA, scalS and scalA refer:
+#'
+#' \deqn{\frac{1}{1 + \exp{\frac{xmidS - t}{scalS}}} - \frac{1}{1 + \exp{\frac{xmidA - t}{scalA}}}}
+#'
+#'
+#' @inheritParams filter_winter
+#' @param year year column name. default is 'yr'.
+#' @param xmidS
+#' @param xmidA
+#' @param scalS
+#' @param scalA
 #'
 #' @return
 #' @export
