3bc6b47ac5743442137e9893c1cee0ff98db96e0
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-23 15:57:50 -0800

    update test-internal

--- inst/tinytest/test-internal.R
+++ inst/tinytest/test-internal.R
@@ -1,37 +1,28 @@
-context("test-internal")
-
+library(data.table)
 
 ndvi <- fread(system.file("extdata", "ndvi.csv", package = "irg"))
 
-
-
 # check_col ---------------------------------------------------------------
-test_that("check_col works", {
-	expect_error(check_col(ndvi, 'potato'),
-							 'potato column not found in DT')
+expect_error(
+	irg:::check_col(ndvi, 'potato'),
+	'potato column not found in DT'
+)
 
-	expect_error(check_col(ndvi, 'potato', arg = 'cheese'),
-							 "column not found in DT",
-							 fixed = FALSE)
-
-})
-
+expect_error(
+	irg:::check_col(ndvi, 'potato', arg = 'cheese'),
+	"column not found in DT",
+	fixed = FALSE
+)
 
 # check_type --------------------------------------------------------------
-test_that("check_type works", {
-	expect_error(
-		check_type(ndvi, 'DayOfYear', 'potato'),
-		'DayOfYear does not match required type: potato'
-	)
-
-})
-
-
+expect_error(
+	irg:::check_type(ndvi, 'DayOfYear', 'potato'),
+	'DayOfYear does not match required type: potato'
+)
 
 # overwrite_col -----------------------------------------------------------
-test_that("overwrite_col works", {
-	copyNDVI <- copy(ndvi)[, potato := 'golden']
-	expect_warning(overwrite_col(copyNDVI, 'potato'),
-								 'overwriting potato column')
-
-})
+copyNDVI <- copy(ndvi)[, potato := 'golden']
+expect_warning(
+	irg:::overwrite_col(copyNDVI, 'potato'),
+	'overwriting potato column'
+)
