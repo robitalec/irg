3e9a346ab48d8b5429a9fe90ec04216917694c47
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-23 16:52:50 -0800

    copy new for each

--- inst/tinytest/test-flex-colnames.R
+++ inst/tinytest/test-flex-colnames.R
@@ -10,12 +10,15 @@ old_names <- colnames(ndvi_new_names)
 setnames(ndvi_new_names,
 				 old_names,
 				 c('identity', 'year', 'day_of_year', 'ndvi', 'qa'))
+ndvi_new_names_raw <- copy(ndvi_new_names)
 
 # Filter QA
+ndvi_new_names <- copy(ndvi_new_names_raw)
 expect_equal(nrow(filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')),
 						 nrow(ndvi_new_names))
 
 # Filter winter
+ndvi_new_names <- copy(ndvi_new_names_raw)
 expect_equal(nrow({
 	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
 	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
@@ -24,6 +27,7 @@ expect_equal(nrow({
 	nrow(ndvi_new_names))
 
 # Filter roll
+ndvi_new_names <- copy(ndvi_new_names_raw)
 expect_equal(nrow({
 	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
 	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
@@ -32,8 +36,9 @@ expect_equal(nrow({
 	nrow(ndvi_new_names))
 
 # Filter top
+ndvi_new_names <- copy(ndvi_new_names_raw)
 expect_equal(nrow({
-	filter_qa(ndvi, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
+	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
 	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
 								doy = 'day_of_year', id = 'identity')
 	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
