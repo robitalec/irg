33c059f220d3d3cc0c1d863dfd402da7c3e55a5f
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-23 17:10:36 -0800

    test flex model_params and model_ndvi

--- inst/tinytest/test-flex-colnames.R
+++ inst/tinytest/test-flex-colnames.R
@@ -61,10 +61,67 @@ expect_equal(nrow({
 								doy = 'day_of_year', id = 'identity')
 	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
 	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
-	scale_doy(ndvi, doy = 'day_of_year')
-	scale_ndvi(ndvi)
-	model_start(ndvi, id = 'identity', year = 'year')
+	scale_doy(ndvi_new_names, doy = 'day_of_year')
+	scale_ndvi(ndvi_new_names)
+	model_start(ndvi_new_names, id = 'identity', year = 'year')
 	}),
 	nrow(ndvi_new_names)
+)
+
+ndvi_new_names <- copy(ndvi_new_names_raw)
+expect_equal(nrow({
+	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
+	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
+								doy = 'day_of_year', id = 'identity')
+	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
+	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
+	scale_doy(ndvi_new_names, doy = 'day_of_year')
+	scale_ndvi(ndvi_new_names)
+	model_start(ndvi_new_names, id = 'identity', year = 'year')
+	# Option 1 - returns = 'models'
+	mods <- model_params(
+		ndvi_new_names,
+		id = 'identity',
+		year = 'year',
+		returns = 'models',
+		xmidS = 'xmidS_start',
+		xmidA = 'xmidA_start',
+		scalS = 0.05,
+		scalA = 0.01
+	)
+
+	# Fit to the whole year (requires assignment)
+	fit <- model_ndvi(mods, observed = FALSE)
+}),
+nrow(ndvi_new_names)
+)
+
+
+ndvi_new_names <- copy(ndvi_new_names_raw)
+expect_equal(nrow({
+	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
+	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
+								doy = 'day_of_year', id = 'identity')
+	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
+	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
+	scale_doy(ndvi_new_names, doy = 'day_of_year')
+	scale_ndvi(ndvi_new_names)
+	model_start(ndvi_new_names, id = 'identity', year = 'year')
+	# Option 2 - returns = 'columns'
+	model_params(
+		ndvi_new_names,
+		id = 'identity',
+		year = 'year',
+		returns = 'columns',
+		xmidS = 'xmidS_start',
+		xmidA = 'xmidA_start',
+		scalS = 0.05,
+		scalA = 0.01
+	)
+
+	# Fit double logistic curve to NDVI time series for the observed days
+	model_ndvi(ndvi_new_names, observed = TRUE)
+}),
+nrow(ndvi_new_names)
 )
 
