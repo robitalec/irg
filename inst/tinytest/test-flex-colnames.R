17bc545ae1c84f3dca2c82935fdc0c0ebc9f0577
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-23 16:50:28 -0800

    fix test nrow, use ndvi_new_names

--- inst/tinytest/test-flex-colnames.R
+++ inst/tinytest/test-flex-colnames.R
@@ -12,34 +12,35 @@ setnames(ndvi_new_names,
 				 c('identity', 'year', 'day_of_year', 'ndvi', 'qa'))
 
 # Filter QA
-expect_equal(filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa'),
+expect_equal(nrow(filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')),
 						 nrow(ndvi_new_names))
 
 # Filter winter
-expect_equal({
+expect_equal(nrow({
 	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
-	filter_winter(ndvi, probs = 0.025, limits = c(60L, 300L),
+	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
 								doy = 'day_of_year', id = 'identity')
-	},
+	}),
 	nrow(ndvi_new_names))
 
 # Filter roll
-expect_equal({
+expect_equal(nrow({
 	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
-	filter_winter(ndvi, probs = 0.025, limits = c(60L, 300L),
+	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
 								doy = 'day_of_year', id = 'identity')
-	},
+	}),
 	nrow(ndvi_new_names))
 
 # Filter top
-expect_equal({
+expect_equal(nrow({
 	filter_qa(ndvi, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
-	filter_winter(ndvi, probs = 0.025, limits = c(60L, 300L),
+	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
 								doy = 'day_of_year', id = 'identity')
-	filter_roll(ndvi, window = 3L, id = 'identity')
-	filter_top(ndvi, probs = 0.925, id = 'identity')
-},
-	nrow(ndvi_new_names))
+	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
+	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
+	}),
+	nrow(ndvi_new_names)
+)
 
-# scale_doy(ndvi)
-# scale_ndvi(ndvi)
+# scale_doy(ndvi_new_names)
+# scale_ndvi(ndvi_new_names)
