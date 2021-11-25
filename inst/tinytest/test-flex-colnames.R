46f9a3cb31bed5d4ed963e5f11379005a4dc9f78
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-24 18:04:33 -0800

    add sections

--- inst/tinytest/test-flex-colnames.R
+++ inst/tinytest/test-flex-colnames.R
@@ -1,10 +1,18 @@
+# Test column names are flexible
+
+
+# Packages ----------------------------------------------------------------
 library(data.table)
 library(irg)
 
+
+
+# Data --------------------------------------------------------------------
 ndvi <- fread(system.file("extdata", "ndvi.csv", package = "irg"))[yr < 2007]
 
 
-# Test column names are flexible
+
+# Change names ------------------------------------------------------------
 id <- 'identity'
 yr <- 'year'
 ndvi_new_names <- copy(ndvi)
@@ -14,6 +22,9 @@ setnames(ndvi_new_names,
 				 c(id, yr, 'day_of_year', 'ndvi', 'qa'))
 ndvi_new_names_raw <- copy(ndvi_new_names)
 
+
+
+# Filter ------------------------------------------------------------------
 # Filter QA
 filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
 expect_equal(nrow(ndvi_new_names),
@@ -35,11 +46,17 @@ filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
 expect_equal(nrow(ndvi_new_names),
 						 nrow(ndvi_new_names_raw))
 
+
+
+# Scale -------------------------------------------------------------------
 # Scale
 scale_doy(ndvi_new_names, doy = 'day_of_year')
 expect_equal(nrow(ndvi_new_names),
 						 nrow(ndvi_new_names_raw))
 
+
+
+# Model -------------------------------------------------------------------
 # Model
 scale_ndvi(ndvi_new_names)
 model_start(ndvi_new_names, id = 'identity', year = 'year')
@@ -83,3 +100,4 @@ model_ndvi(ndvi_new_names, observed = TRUE)
 expect_equal(nrow(ndvi_new_names),
 						 nrow(ndvi_new_names_raw))
 
+# irg ---------------------------------------------------------------------
