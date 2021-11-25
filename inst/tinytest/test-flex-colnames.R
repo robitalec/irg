1e9aa32835ab2a343f2902cf1f1fea62fe526124
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-24 17:35:05 -0800

    simplify flex columns tests

--- inst/tinytest/test-flex-colnames.R
+++ inst/tinytest/test-flex-colnames.R
@@ -15,115 +15,71 @@ setnames(ndvi_new_names,
 ndvi_new_names_raw <- copy(ndvi_new_names)
 
 # Filter QA
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow(filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')),
-						 nrow(ndvi_new_names))
+filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
 
 # Filter winter
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow({
-	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
-	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
-								doy = 'day_of_year', id = 'identity')
-	}),
-	nrow(ndvi_new_names))
+filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
+							doy = 'day_of_year', id = 'identity')
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
 
 # Filter roll
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow({
-	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa')
-	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
-								doy = 'day_of_year', id = 'identity')
-	}),
-	nrow(ndvi_new_names))
+filter_roll(ndvi_new_names, window = 3L, id = 'identity')
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
 
 # Filter top
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow({
-	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
-	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
-								doy = 'day_of_year', id = 'identity')
-	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
-	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
-	}),
-	nrow(ndvi_new_names)
-)
+filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
 
 # Scale
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow(scale_doy(ndvi_new_names, doy = 'day_of_year')),
-						 nrow(ndvi_new_names))
-
+scale_doy(ndvi_new_names, doy = 'day_of_year')
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
 
 # Model
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow({
-	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
-	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
-								doy = 'day_of_year', id = 'identity')
-	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
-	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
-	scale_doy(ndvi_new_names, doy = 'day_of_year')
-	scale_ndvi(ndvi_new_names)
-	model_start(ndvi_new_names, id = 'identity', year = 'year')
-	}),
-	nrow(ndvi_new_names)
+scale_ndvi(ndvi_new_names)
+model_start(ndvi_new_names, id = 'identity', year = 'year')
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
+
+# Option 1 - returns = 'models'
+mods <- model_params(
+	ndvi_new_names,
+	id = 'identity',
+	year = 'year',
+	returns = 'models',
+	xmidS = 'xmidS_start',
+	xmidA = 'xmidA_start',
+	scalS = 0.05,
+	scalA = 0.01
 )
 
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(colnames({
-	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
-	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
-								doy = 'day_of_year', id = 'identity')
-	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
-	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
-	scale_doy(ndvi_new_names, doy = 'day_of_year')
-	scale_ndvi(ndvi_new_names)
-	model_start(ndvi_new_names, id = 'identity', year = 'year')
-	# Option 1 - returns = 'models'
-	mods <- model_params(
-		ndvi_new_names,
-		id = 'identity',
-		year = 'year',
-		returns = 'models',
-		xmidS = 'xmidS_start',
-		xmidA = 'xmidA_start',
-		scalS = 0.05,
-		scalA = 0.01
-	)
+# Fit to the whole year (requires assignment)
+fit <- model_ndvi(mods, observed = FALSE)
 
-	# Fit to the whole year (requires assignment)
-	fit <- model_ndvi(mods, observed = FALSE)
-}),
-	c(id, yr, 'xmidS', 'xmidA', 'scalS', 'scalA', 'nls_error', 't', 'fitted')
+expect_equal(colnames(fit),
+						 c(id, yr, 'xmidS', 'xmidA', 'scalS', 'scalA',
+						 	'nls_error', 't', 'fitted'))
+
+
+# Option 2 - returns = 'columns'
+model_params(
+	ndvi_new_names,
+	id = 'identity',
+	year = 'year',
+	returns = 'columns',
+	xmidS = 'xmidS_start',
+	xmidA = 'xmidA_start',
+	scalS = 0.05,
+	scalA = 0.01
 )
 
-
-ndvi_new_names <- copy(ndvi_new_names_raw)
-expect_equal(nrow({
-	filter_qa(ndvi_new_names, ndvi = 'ndvi', qa = 'qa', good = c(0, 1))
-	filter_winter(ndvi_new_names, probs = 0.025, limits = c(60L, 300L),
-								doy = 'day_of_year', id = 'identity')
-	filter_roll(ndvi_new_names, window = 3L, id = 'identity')
-	filter_top(ndvi_new_names, probs = 0.925, id = 'identity')
-	scale_doy(ndvi_new_names, doy = 'day_of_year')
-	scale_ndvi(ndvi_new_names)
-	model_start(ndvi_new_names, id = 'identity', year = 'year')
-	# Option 2 - returns = 'columns'
-	model_params(
-		ndvi_new_names,
-		id = 'identity',
-		year = 'year',
-		returns = 'columns',
-		xmidS = 'xmidS_start',
-		xmidA = 'xmidA_start',
-		scalS = 0.05,
-		scalA = 0.01
-	)
-
-	# Fit double logistic curve to NDVI time series for the observed days
-	model_ndvi(ndvi_new_names, observed = TRUE)
-}),
-nrow(ndvi_new_names)
-)
+# Fit double logistic curve to NDVI time series for the observed days
+model_ndvi(ndvi_new_names, observed = TRUE)
+expect_equal(nrow(ndvi_new_names),
+						 nrow(ndvi_new_names_raw))
 
