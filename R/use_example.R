8605ff6ec5760d123e1f54bb7cc92514334f7f6f
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-30 15:09:40 -0800

    fst use_example_modis

---
+++ R/use_example.R
@@ -0,0 +1,30 @@
+#' Use an example MODIS MOD13Q1 sampling script
+#'
+#' @param save boolean indicating if the example script should be saved
+#' @param location file name indicating where to save the example script.
+#' Default is "./Sample-NDVI-For-IRG-MODIS.js"
+#' @param overwrite
+#'
+#' @return
+#' @export
+#'
+#' @examples
+use_example_extract_ndvi <- function(sensor = 'MODIS', save = FALSE,
+																		 location = NULL, overwrite = FALSE) {
+
+	# stop if overwrite = FALSE and exists
+
+	# if sensor MODIS
+	# set save path
+
+	# if sensor Landsat
+	# set save path
+
+	# else error
+
+	# save to path
+
+
+
+
+}
