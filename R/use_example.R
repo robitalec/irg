37b1f492eb5decb9e779fe1bcea0f4746a6d357f
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-30 15:45:35 -0800

    add sensor swap logic

--- R/use_example.R
+++ R/use_example.R
@@ -1,7 +1,8 @@
 #' Use an example MODIS MOD13Q1 sampling script
 #'
+#' @param sensor either 'MODIS' or 'Landsat'
 #' @param save boolean indicating if the example script should be saved
-#' @param location file name indicating where to save the example script.
+#' @param filename file name indicating where to save the example script.
 #' Default is "./Sample-NDVI-For-IRG-MODIS.js"
 #' @param overwrite
 #'
@@ -10,21 +11,30 @@
 #'
 #' @examples
 use_example_extract_ndvi <- function(sensor = 'MODIS', save = FALSE,
-																		 location = NULL, overwrite = FALSE) {
-
-	# stop if overwrite = FALSE and exists
-
-	# if sensor MODIS
-	# set save path
-
-	# if sensor Landsat
-	# set save path
-
-	# else error
-
-	# save to path
+																		 filename = NULL, overwrite = FALSE) {
 
 
+	if (save & !is.nulL(filename) & file.exists(filename) & overwrite) {
+		stop('found file at ', filename,
+				 '- set overwrite to TRUE if you would like to save there anyways')
+	}
 
+	if (sensor == 'MODIS') {
+		if (is.null(filename)) {
+			filename <- 'Sample-NDVI-For-IRG-MODIS.js'
+		}
+		file.copy(from = system.file('javascript', 'Sample-NDVI-For-IRG-MODIS.js',
+																 package = 'irg'),
+							to = filename)
+	} else if (sensor == 'Landsat') {
+		if (is.null(filename)) {
+			filename <- 'Sample-NDVI-For-IRG-Landsat.js'
+		}
+		file.copy(from = system.file('javascript', 'Sample-NDVI-For-IRG-Landsat.js',
+																 package = 'irg'),
+							to = filename)
+	} else {
+		stop('sensor must be one of: "MODIS" or "Landsat"')
+	}
 
 }
