1fe29cfcf38e06cf7888aea1935a2eacb2e8a60f
Alec L. Robitaille <robit.alec@gmail.com>
2021-12-01 15:33:54 -0800

    update test-model with chk

--- inst/tinytest/test-model.R
+++ inst/tinytest/test-model.R
@@ -11,33 +11,23 @@ scale_ndvi(ndvi)
 # model_start -------------------------------------------------------------
 ## column checks
 copyNDVI <- copy(ndvi)[, scaled := NULL]
-expect_error(
-	model_start(copyNDVI),
-	'scaled column not found in DT - did you filter and scale?'
-)
+expect_error(model_start(copyNDVI),
+						 'must include', fixed = FALSE)
 
 copyNDVI <- copy(ndvi)[, t := NULL]
-expect_error(
-	model_start(copyNDVI),
-	't column not found in DT - did you scale doy?'
-)
+expect_error(model_start(copyNDVI),
+						 'must include', fixed = FALSE)
 
 copyNDVI <- copy(ndvi)[, id := NULL]
-expect_error(
-	model_start(copyNDVI),
-	"id \\('id'\\) column not found in DT"
-)
+expect_error(model_start(copyNDVI),
+						 'must include', fixed = FALSE)
 
 copyNDVI <- copy(ndvi)[, yr := NULL]
-expect_error(
-	model_start(copyNDVI),
-	"year \\('yr'\\) column not found in DT"
-)
+expect_error(model_start(copyNDVI),
+						 'must include', fixed = FALSE)
 
-expect_true(all(
-	c('id', 'yr', 'xmidS_start', 'xmidA_start') %in%
-		colnames(model_start(ndvi))
-))
+expect_true(all(c('id', 'yr', 'xmidS_start', 'xmidA_start') %in%
+									colnames(model_start(ndvi))))
 
 
 # model_params ------------------------------------------------------------
@@ -47,34 +37,28 @@ expect_error(
 	fixed = FALSE
 )
 
-expect_error(
-	model_params(ndvi, xmidS = 'potato'),
-	"xmidS \\('potato'\\) column not found in DT"
-)
+expect_error(model_params(ndvi, xmidS = 'potato'),
+						 'must include', fixed = FALSE)
 
-expect_error(
-	model_params(ndvi, xmidS = 'xmidS_start', xmidA = 'potato'),
-	"xmidA \\('potato'\\) column not found in DT"
-)
+expect_error(model_params(ndvi, xmidS = 'xmidS_start', xmidA = 'potato'),
+						 'must include', fixed = FALSE)
 
-expect_error(
-	model_params(ndvi, xmidS = 'xmidS_start', xmidA = 'xmidA_start',
-							 scalS = 'potato'),
-	"scalS \\('potato'\\) column not found in DT"
-)
+expect_error(model_params(ndvi, xmidS = 'xmidS_start', xmidA = 'xmidA_start',
+													scalS = 'potato'),
+						 'must include', fixed = FALSE)
 
 expect_error(
 	model_params(ndvi, xmidS = 'xmidS_start', xmidA = 'xmidA_start',
 							 scalS = 'id', #just to skip to scalA
 							 scalA = 'potato'),
-	"scalA \\('potato'\\) column not found in DT"
+	'must include', fixed = FALSE
 )
 
 expect_error(
 	model_params(ndvi, xmidS = 'xmidS_start', xmidA = 'xmidA_start',
 							scalS = 'id', #just to skip to returns
 							scalA = 'id'), #just to skip to returns
-	'argument "returns" is NULL, must provide one of "models" or "columns"'
+	'must not be NULL', fixed = FALSE
 )
 
 mods <- model_params(
@@ -118,25 +102,25 @@ model_params(
 copyNDVI <- copy(ndvi)[, xmidS := NULL]
 expect_error(
 	model_ndvi(copyNDVI),
-	'xmidS column not found in DT'
+	'must include', fixed = FALSE
 )
 
 copyNDVI <- copy(ndvi)[, xmidA := NULL]
 expect_error(
 	model_ndvi(copyNDVI),
-	'xmidA column not found in DT'
+	'must include', fixed = FALSE
 )
 
 copyNDVI <- copy(ndvi)[, scalS := NULL]
 expect_error(
 	model_ndvi(copyNDVI),
-	'scalS column not found in DT'
+	'must include', fixed = FALSE
 )
 
 copyNDVI <- copy(ndvi)[, scalA := NULL]
 expect_error(
 	model_ndvi(copyNDVI),
-	'scalA column not found in DT'
+	'must include', fixed = FALSE
 )
 
 modNDVI <- model_ndvi(ndvi, observed = FALSE)
